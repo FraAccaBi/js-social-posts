64576f7edff713d5492a3512c65319db2a1224a9
FraAccaBi <francescahuda@gmail.com>
2022-04-16 00:20:50 +0200

    backup commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const post = [
         nome: 'Phil Mangione',
         foto: 'https://unsplash.it/300/300?image=',
         data: '05/22/2021',
-        testo: 'lorem',
+        testo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab eveniet dolore sunt explicabo provident fugit saepe accusamus sit fugiat assumenda! Accusantium ducimus laborum aut architecto soluta quia, temporibus nulla voluptates.',
         immagine: 'https://unsplash.it/300/300?image=',
         likes: 80
     },
@@ -27,36 +27,72 @@ const post = [
         nome: 'Sofia Perlari',
         foto: 'https://unsplash.it/300/300?image=',
         data: '01/08/2021',
-        testo: 'lorem',
+        testo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab eveniet dolore sunt explicabo provident fugit saepe accusamus sit fugiat assumenda! Accusantium ducimus laborum aut architecto soluta quia, temporibus nulla voluptates.',
         immagine: 'https://unsplash.it/300/300?image=',
         likes: 120
     }
 ]
 
-console.log(post);
+//console.log(post);
 
 /* Milestone 2
 Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
  */
 
-let card_1 = document.getElementById('1');
+//seleziono un elemento della dom a cui associo l'id del primo object della variabile post
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//appendo all'elemento selezionato un div con il markup per visualizzare *immagine di profilo  *nome *testo *immagine *bottone likes *numero likes
+let element_likes = []
+let i = 0
+while (i < post.length) {
+    let element = post[i]
+    let card = document.getElementById(i);
+    element_likes.push(element.likes)
+    let markup = 
+    `
+        <div class="header d-flex row row-cols-2">
+            <div class="profile_img col">
+                <img class="profile_img"
+                src="${element.foto}"
+                alt="">
+            </div>
+            <div class="col">
+                <div class="name">${element.nome} </div>
+                <div class="data">${element.data}</div>
+            </div>            
+        </div>
+        <div class="main">
+            <div class="testo">${element.testo}</div>
+            <div class="foto">
+                <img class="main_img"
+                src="${element.immagine}"
+                alt="">
+            </div>
+        </div>
+        <div class="bottoni d-flex row row-cols-2 text-center">
+            <div class="likes col">
+            <i class="fa-solid fa-thumbs-up"></i> Mi piace
+            </div>
+            <div class="numero_likes col">Piace a ${element.likes} persone</div>
+        </div>
+        </div>
+    `
+card.innerHTML = markup
+i++
+}
+console.log(element_likes);
 /* Milestone 3
 Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like. */
+
+let likes = document.querySelector('.likes')
+let numero_likes = document.querySelector('.numero_likes')
+
+
+likes.addEventListener('click', function(){
+    element_likes[0] += 1
+    numero_likes.innerHTML = `Piace a ${element_likes[0]} persone` 
+})
+
